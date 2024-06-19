d017f3ebd577a208571d35739628a7168e069d3e
Anurag Joshi <148903459+anuragJoshi05@users.noreply.github.com>
2024-06-19 11:17:26 +0530

    Delete ccs.dart

--- ccs.dart
+++
@@ -1,9 +0,0 @@
-FirebaseAuth.instance
-  .authStateChanges()
-  .listen((User? user) {
-    if (user == null) {
-      print('User is currently signed out!');
-    } else {
-      print('User is signed in!');
-    }
-  });
