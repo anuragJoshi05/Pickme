f95abb34f17ba781e8df6ec208fa45badc21db62
Anurag Joshi <148903459+anuragJoshi05@users.noreply.github.com>
2024-06-19 11:18:44 +0530

    Delete lib/branch.dart

--- lib/branch.dart
+++
@@ -1,8 +0,0 @@
-Future<void> main() async {
-WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
-
-// Ideal time to initialize
-await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-//...
-}
